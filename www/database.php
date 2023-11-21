d02b7b55ec0a789ed9913576dca3fcca48732f06
Bekir Sezgin <162413@novacollege.nl>
2023-11-21 12:55:41 +0100

    begin

---
+++ www/database.php
@@ -0,0 +1,11 @@
+<?php
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "mobile4you";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+?>
--- www/index.php
+++ www/index.php
@@ -1,3 +1,3 @@
 <?php
 
-// Dit is de eerste pagina van de webshop
+require 'database.php';
