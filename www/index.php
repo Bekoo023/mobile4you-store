bb8ffb40dd2a069b144a028924d1e11b06b73d96
Bekir Sezgin <162413@novacollege.nl>
2023-11-21 12:57:40 +0100

    link

--- www/index.php
+++ www/index.php
@@ -1,3 +1,18 @@
 <?php
 
 require 'database.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Mobile4You</title>
+</head>
+<body>
+    
+</body>
+</html>
